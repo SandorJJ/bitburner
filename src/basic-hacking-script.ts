091284d2aa3a288a11af55976abbbd7f1815dd96
Sandor Jonas <SandorJanosJonas@gmail.com>
2023-04-23 14:25:56 -0500

    added while loop

--- src/basic-hacking-script.ts
+++ src/basic-hacking-script.ts
@@ -5,14 +5,16 @@ export async function main(ns: NS) {
   const moneyThreshold = ns.getServerMaxMoney(target) * 0.8;
   const securityThreshold = ns.getServerMinSecurityLevel(target) * 1.2;
   
-  if (ns.getServerMoneyAvailable(target) < moneyThreshold) {
-    let grownBy = await ns.grow(target);
-    ns.print("INFO: " + target + "'s money was grown by " + grownBy);
-  } else if (ns.getServerSecurityLevel(target) > securityThreshold) {
-    let weakenedBy = await ns.weaken(target);
-    ns.print("INFO: " + target + " was weakened by " + weakenedBy); 
-  } else {
-    let moneyStolen = await ns.hack;
-    ns.print("INFO: Money stolen from " + target + ": " + moneyStolen);
+  while (true) {
+    if (ns.getServerMoneyAvailable(target) < moneyThreshold) {
+      let grownBy = await ns.grow(target);
+      ns.print("INFO: " + target + "'s money was grown by " + grownBy);
+    } else if (ns.getServerSecurityLevel(target) > securityThreshold) {
+      let weakenedBy = await ns.weaken(target);
+      ns.print("INFO: " + target + " was weakened by " + weakenedBy); 
+    } else {
+      let moneyStolen = await ns.hack;
+      ns.print("INFO: Money stolen from " + target + ": " + moneyStolen);
+    }
   }
 }
