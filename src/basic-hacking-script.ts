f01c5323d928d0453ac43f7f022df7ed2ef2c7e3
Sandor Jonas <SandorJanosJonas@gmail.com>
2023-04-23 14:16:46 -0500

    first hacking script made

--- src/basic-hacking-script.ts
+++ src/basic-hacking-script.ts
@@ -1,11 +1,18 @@
-/** 
- * @param {NS} ns 
- **/
-
-import { NS } from "../NetscriptDefinitions.js";
+import { NS } from '../NetscriptDefinitions';
 
 export async function main(ns: NS) {
-  ns.tprint('Hello World!');
-  ns.tprint("Test!");
-  ns.tprint("Test 33333!");
+  const target = "" + ns.args[0];
+  const moneyThreshold = ns.getServerMaxMoney(target) * 0.8;
+  const securityThreshold = ns.getServerMinSecurityLevel(target) * 1.2;
+  
+  if (ns.getServerMoneyAvailable(target) < moneyThreshold) {
+    let grownBy = await ns.grow(target);
+    ns.print("INFO: " + target + "'s money was grown by " + grownBy);
+  } else if (ns.getServerSecurityLevel(target) > securityThreshold) {
+    let weakenedBy = await ns.weaken(target);
+    ns.print("INFO: " + target + " was weakened by " + weakenedBy); 
+  } else {
+    let moneyStolen = await ns.hack;
+    ns.print("INFO: Money stolen from " + target + ": " + moneyStolen);
+  }
 }
