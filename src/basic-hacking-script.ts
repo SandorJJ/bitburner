f615ade7c03b8ba4ab3cd9d4a36a75d58d5f95b9
Sandor Jonas <SandorJanosJonas@gmail.com>
2023-04-23 13:18:52 -0500

    initializing repo

---
+++ src/basic-hacking-script.ts
@@ -0,0 +1,11 @@
+/** 
+ * @param {NS} ns 
+ **/
+
+import { NS } from "../NetscriptDefinitions.js";
+
+export async function main(ns: NS) {
+  ns.tprint('Hello World!');
+  ns.tprint("Test!");
+  ns.tprint("Test 33333!");
+}
