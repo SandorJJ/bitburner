35aa79d431aff0ad943c950fb32f146b8d1a7dbc
Sandor Jonas <SandorJanosJonas@gmail.com>
2023-04-23 14:42:17 -0500

    reorder things to see if it fixes crash

--- src/basic-hacking-script.ts
+++ src/basic-hacking-script.ts
@@ -2,19 +2,20 @@ import { NS } from '../NetscriptDefinitions';
 
 export async function main(ns: NS) {
   const target = "" + ns.args[0];
-  const moneyThreshold = ns.getServerMaxMoney(target) * 0.8;
-  const securityThreshold = ns.getServerMinSecurityLevel(target) * 1.2;
+  const moneyThreshold = ns.getServerMaxMoney(target) * 0.75;
+  const securityThreshold = ns.getServerMinSecurityLevel(target) + 5;
   
   while (true) {
-    if (ns.getServerMoneyAvailable(target) < moneyThreshold) {
-      let grownBy = await ns.grow(target);
-      ns.print("INFO: " + target + "'s money was grown by " + grownBy);
-    } else if (ns.getServerSecurityLevel(target) > securityThreshold) {
+    if (ns.getServerSecurityLevel(target) > securityThreshold) {
       let weakenedBy = await ns.weaken(target);
       ns.print("INFO: " + target + " was weakened by " + weakenedBy); 
+    } else if (ns.getServerMoneyAvailable(target) < moneyThreshold) {
+      let grownBy = await ns.grow(target) - 1 * 100;
+      let securityLevel = ns.getServerSecurityLevel(target);
+      ns.print("INFO: " + target + "'s money was grown by " + grownBy + "% (Security: " + securityLevel + ")");
     } else {
       let moneyStolen = await ns.hack;
-      ns.print("INFO: Money stolen from " + target + ": " + moneyStolen);
+      ns.print("INFO: Money stolen from " + target + ": " + moneyStolen);      
     }
   }
 }
