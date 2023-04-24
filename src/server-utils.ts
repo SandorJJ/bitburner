6f5638e169ff2f14fa203d1913649203a162216f
Sandor Jonas <SandorJanosJonas@gmail.com>
2023-04-23 20:02:42 -0500

    added a description according to JSDoc for the getNetwork function

--- src/server-utils.ts
+++ src/server-utils.ts
@@ -4,6 +4,14 @@ export async function main(ns: NS) {
     ns.print(getNetwork(ns).length);
 }
 
+/**
+ * Scans for all of the servers in the bitburner world.
+ * 
+ * @param {NS} ns - The Netscript API.
+ * @param {string} root - Where the program starts scanning for servers, default is "home".
+ * @param {string[]} visited - The list of servers that have been visited.
+ * @returns {string[]} An array of all the servers.
+ */
 function getNetwork(ns: NS, root: string = "home", visited:string[] = []): string[] {
     ns.scan(root)
     .filter(host => !visited.includes(host))
