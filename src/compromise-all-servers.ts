c9bbc39b1c45326f412c858cc6ba3c3ca44ccea1
Sandor Jonas <SandorJanosJonas@gmail.com>
2023-04-25 20:48:22 -0500

    (new) created compromise-all-servers.ts

---
+++ src/compromise-all-servers.ts
@@ -0,0 +1,24 @@
+import { NS } from "../NetscriptDefinitions";
+import { getNetwork, compromiseServer } from "./server-utils";
+
+export async function main(ns: NS) {
+    const servers = getNetwork(ns)
+        .filter(server => !ns.hasRootAccess(server));
+    
+    let compromised = 0;
+    for (const server of servers) {
+        let value = compromiseServer(ns, server);
+
+        if (value === 0) {
+            compromised++;
+        } else if (value === 2) {
+            ns.tprint("ERROR: An unexpected error has occured.");
+        }
+    }
+
+    if (compromised === 0) {
+        ns.tprint("Unable to compromise more servers.");
+    } else {
+        ns.tprint("SUCCESS: Successfully compromised " + compromised + " servers!");
+    }
+}
