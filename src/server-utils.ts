c8f298a9990b3fb940204d89f17b006f721e320f
Sandor Jonas <SandorJanosJonas@gmail.com>
2023-04-25 20:47:01 -0500

    (new) created compromiseServer() in server-utils.ts

--- src/server-utils.ts
+++ src/server-utils.ts
@@ -33,3 +33,44 @@ export function getNetwork(ns: NS, root: string = "home", visited:string[] = [])
 export function getServerRam(ns: NS, server: string): number {
     return ns.getServerMaxRam(server);
 }
+
+/**
+ * Gains root access to a specific server.
+ * 
+ * @param {NS} ns - The Netscript API. 
+ * @param {string} server - The server to compromise.
+ * @returns {number} Returns 0 if the compromise was successful, 1 if not enough ports have been opened, 2 if an unexpected error occured.
+ */
+export function compromiseServer(ns: NS, server: string): number {
+    if (ns.fileExists("BruteSSH.exe")) {
+        ns.brutessh(server);
+    }
+
+    if (ns.fileExists("FTPCrack.exe")) {
+        ns.ftpcrack(server);
+    }
+
+    if (ns.fileExists("relaySMTP.exe")) {
+        ns.relaysmtp(server);
+    }
+
+    if (ns.fileExists("HTTPWorm.exe")) {
+        ns.httpworm(server);
+    }
+
+    if (ns.fileExists("SQLInject")) {
+        ns.sqlinject(server);
+    }
+    
+    try {
+        ns.nuke(server);
+    } catch (error) {
+        if (error.includes("Not enough ports opened to use NUKE.exe virus.")) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+
+    return 0;
+}
