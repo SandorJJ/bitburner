5c8ff7aa0ca3e13eb9d24aa993846ae736446fb5
Sandor Jonas <SandorJanosJonas@gmail.com>
2023-04-28 21:27:28 -0500

    (update) continued working on setup-split-hacking.ts

--- src/setup-split-hacking.ts
+++ src/setup-split-hacking.ts
@@ -5,13 +5,37 @@ export async function main(ns: NS) {
     const script = "basic-hacking-script.js";
     const scriptRam = ns.getScriptRam(script);
     const serversWithRam = getNetwork(ns).filter(server => ns.getServerMaxRam(server) > 0);
-    const serversWithMoney = getNetwork(ns).filter(server => ns.getServerMaxMoney(server) > 0);
+    const hackableServers = getNetwork(ns)
+        .filter(server => ns.getServerMaxMoney(server) > 0)
+        .filter(server => ns.getServerRequiredHackingLevel(server) < (ns.getHackingLevel() / 3));
+    let totalRam = 0;
+    for (const server of serversWithRam) {
+        totalRam += ns.getServerMaxRam(server);
+    }
+    const threadsPerServer = Math.floor(totalRam / scriptRam / hackableServers.length);
 
 
-    for (const serverWithMoney of serversWithMoney) {
+    for (const hackableServer of hackableServers) {
+        let threadsLeft = threadsPerServer;
+
         for (const serverWithRam of serversWithRam) {
-            let serverRam = ns.getServerMaxRam(serverWithRam);
+            let serverRamAvailable = ns.getServerMaxRam(serverWithRam) - ns.getServerUsedRam(serverWithRam);
+            let threads = Math.floor(serverRamAvailable / scriptRam);
+
+            if (threadsLeft < threads) {
+                threads = threadsLeft;
+            }
             
+            ns.exec(script, serverWithRam, threads, hackableServer);
+            threadsLeft -= threads;
+
+            if (serverRamAvailable < scriptRam) {
+                serversWithRam.shift();
+            }
+
+            if (threads === 0) {
+                return;
+            }
         }
     }
 }
