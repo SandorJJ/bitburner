24f71eb553cdc0c540144234c1206e4b11339d34
Sandor Jonas <SandorJanosJonas@gmail.com>
2023-04-30 20:37:57 -0500

    (new) created killScripts() in terminate.ts

---
+++ src/terminate.ts
@@ -0,0 +1,17 @@
+import { NS } from '../NetscriptDefinitions';
+import { getNetworkWithScripts } from './network';
+
+export async function main(ns: NS) {
+
+}
+
+
+/**
+ * Kills all running scripts of an array of servers. Kills all scripts by default.
+ * 
+ * @param {NS} ns - The Netscript API.
+ * @param {string[]} servers - An array of servers to kill scripts on, default: all servers with running scripts.
+ */
+export function killAllScripts(ns: NS, servers: string[] = getNetworkWithScripts(ns)): void {
+    servers.forEach((server: string) => ns.killall(server, true));
+}
