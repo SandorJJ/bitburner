1a9339abaa50493e79281f78caf06e28cfe5ece5
SandorJJ <sandorjanosjonas@gmail.com>
2023-08-08 19:58:45 -0500

    tried to add indenting to map.ts

--- src/server/map.ts
+++ src/server/map.ts
@@ -7,19 +7,29 @@ export async function main(ns: NS) {
     printServers(ns, servers);
 }
 
-function printServers(ns: NS, servers: string[], printed: string[] = ["home"]): void {
+function printServers(ns: NS, servers: string[], printed: string[] = ["home"], indent: number = 0): void {
     printed = printed.concat(servers);
     for (let i = 0; i < servers.length; i++) {
+        ns.tprintf(addIndent(indent, "┃"));
         if (i === servers.length - 1) {
-            ns.tprintf("┃\n┗" + servers[i]);
+            ns.tprintf(addIndent(indent, "┗" + servers[i]));
         } else {
-            ns.tprintf("┃\n┣" + servers[i]);
+            ns.tprintf(addIndent(indent, "┣" + servers[i]));
         }
 
         const connectedServers = ns.scan(servers[i]).filter((server) => !printed.includes(server));
         if (connectedServers.length > 0) {
-            printServers(ns, connectedServers, printed);
+            printServers(ns, connectedServers, printed, indent = indent + 1);
         }
 
     }
+}
+
+function addIndent(level: number, string: string) {
+    let indent = "";
+    for (let i = 0; i < level; i++) {
+        indent += " ";
+    }
+
+    return indent + string;
 }
