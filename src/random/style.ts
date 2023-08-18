d4357daf9913c6382c8aa7287db1f4bf162d5581
SandorJJ <sandorjanosjonas@gmail.com>
2023-08-17 20:35:01 -0500

    organized style.ts

--- src/random/style.ts
+++ src/random/style.ts
@@ -1,5 +1,9 @@
+// Colors
 export const RED = "\u001b[31m";
 export const GREEN = "\u001b[32m";
 export const CYAN = "\u001b[36m";
+
+// Graphics
 export const UNDERLINE = "\u001b[4m";
+
 export const RESET = "\u001b[0m";
