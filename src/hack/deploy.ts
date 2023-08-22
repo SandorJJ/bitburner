9336b23c9f4c02a789650cebf9e70c79544663a1
SandorJJ <sandorjanosjonas@gmail.com>
2023-08-21 20:02:10 -0500

    redid script usage in hack.ts and deploy.ts

--- src/hack/deploy.ts
+++ src/hack/deploy.ts
@@ -2,36 +2,36 @@ import { NS } from "../../NetscriptDefinitions";
 import { YELLOW, RESET } from "../random/style";
 import { getServers } from "../server/network";
 
-const SCRIPT = "hack/hack.ts";
+const SCRIPT = "hack/hack.js";
 
 export async function main(ns: NS) {
-    if (ns.args.length != 1 && ns.args.length != 2) {
+    if (ns.args.length != 1) {
         ns.tprintf(`${YELLOW}Wrong number of arguments entered!${RESET}`);
+        ns.tprintf("Deploy.ts: deploys hack.ts on all possible servers.\n" +
+                   "Usage: deploy.ts target\n" +
+                   "target        the target for hack.ts")
         return;
     }
 
-    const option = String(ns.args[0]);
-    if (option === "-h" || option === "--help") {
-        ns.tprintf("Deploy.ts: deploys hack.ts on all possible servers.\n" +
-                   "Usage: deploy.ts (-h)(-d deploy)\n" +
-                   "-h    --help        displays help information\n" +
-                   "-d    --deploy      start deploying hack.ts on all possible servers")
-    } else if (option === "-d" || option === "--deploy") {
-        const target = String(ns.args[1]);
-        const scriptRam = ns.getScriptRam(SCRIPT);
-        const rootAccessServers = getServers(ns).filter((server) => ns.hasRootAccess(server))
-        .filter((server) => ns.getServerMaxRam(server) > scriptRam);
+    const target = String(ns.args[0]);
+    try {
+        ns.hasRootAccess(target);
+    } catch (error) {
+        ns.tprintf(`${YELLOW}Invalid target entered!${RESET}`);
+        return;
+    }
+
+    const scriptRam = ns.getScriptRam(SCRIPT);
+    const rootAccessServers = getServers(ns).filter((server) => ns.hasRootAccess(server))
+    .filter((server) => ns.getServerMaxRam(server) > scriptRam);
 
 
-        for (const server of rootAccessServers) {
-            const serverRam = ns.getServerMaxRam(server);
-            const threads = Math.floor(serverRam / scriptRam);
+    for (const server of rootAccessServers) {
+        const serverRam = ns.getServerMaxRam(server);
+        const threads = Math.floor(serverRam / scriptRam);
 
-            ns.scp(SCRIPT, server, "home");
-            ns.exec(SCRIPT, server, threads, "--hack", target);
-            await ns.sleep(15000);
-        }
-    } else {
-        ns.tprintf(`${YELLOW}Invalid usage!${RESET}`);
+        ns.scp(SCRIPT, server, "home");
+        ns.exec(SCRIPT, server, threads, target);
+        await ns.sleep(15000);
     }
 }
--- src/hack/hack.ts
+++ src/hack/hack.ts
@@ -2,39 +2,32 @@ import { NS } from "../../NetscriptDefinitions";
 import { YELLOW, RESET } from "../random/style";
 
 export async function main(ns: NS) {
-    if (ns.args.length != 1 && ns.args.length != 2) {
+    if (ns.args.length != 1) {
         ns.tprintf(`${YELLOW}Wrong number of arguments entered!${RESET}`);
+        ns.tprintf("Hack.ts: grows, weakens, and hacks a target forever.\n" +
+                   "Usage: hack.ts target\n" +
+                   "target        the server to grow, weaken, and hack")
         return;
     }
     
-    const option = String(ns.args[0]);
-    if (option === "-h" || option === "--help") {
-        ns.tprintf("Hack.ts: grows, weakens, and hacks a target forever.\n" +
-                   "Usage: hack.ts (-h)(-a target)\n" +
-                   "-h    --help        displays help information\n" +
-                   "-a    --hack        start growing, weakening, and hacking the target")
-    } else if (option === "-a" || option === "--hack") {
-        const target = String(ns.args[1]);
-        try {
-            ns.getServerMinSecurityLevel(target);
-        } catch (error) {
-            ns.tprintf(`${YELLOW}Invalid target entered!${RESET}`);
-            return;
-        }
+    const target = String(ns.args[1]);
+    try {
+        ns.getServerMinSecurityLevel(target);
+    } catch (error) {
+        ns.tprintf(`${YELLOW}Invalid target entered!${RESET}`);
+        return;
+    }
 
-        const securityThreshold = ns.getServerMinSecurityLevel(target) * 1.1;
-        const moneyThreshold = ns.getServerMaxMoney(target) * 0.90;
+    const securityThreshold = ns.getServerMinSecurityLevel(target) * 1.1;
+    const moneyThreshold = ns.getServerMaxMoney(target) * 0.90;
 
-        while (true) {
-            if (securityThreshold < ns.getServerSecurityLevel(target)) {
-                await ns.weaken(target);
-            } else if (moneyThreshold > ns.getServerMoneyAvailable(target)) {
-                await ns.grow(target);
-            } else {
-                await ns.hack(target);
-            }
+    while (true) {
+        if (securityThreshold < ns.getServerSecurityLevel(target)) {
+            await ns.weaken(target);
+        } else if (moneyThreshold > ns.getServerMoneyAvailable(target)) {
+            await ns.grow(target);
+        } else {
+            await ns.hack(target);
         }
-    } else {
-        ns.tprintf(`${YELLOW}Invalid usage!${RESET}`);
     }
 }
